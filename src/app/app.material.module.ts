c7634784f5b54ed2005b7df8267b0bb06685d8fc
Doug Kent <doug.kent88@gmail.com>
2019-06-18 18:51:58 -0500

    upgrading angular

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
-import {
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatGridListModule,
-    MatButtonModule,
-    MatTableModule,
-    MatSortModule,
-    MatCheckboxModule,
-    MatTooltipModule,
-    MatSnackBarModule,
-    MatExpansionModule,
-    MatPaginatorModule,
-    MatInputModule,
-    MatDialogModule,
-    MatIconModule,
-  } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   imports: [ 

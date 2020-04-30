48542ecc862632177516af2e83f25a26bc649667
Doug Kent <doug.kent88@gmail.com>
2020-04-30 07:00:11 -0500

    chore: removing mtf section and adding link to mtf.org

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,9 +1,3 @@
 import { NavBarComponent } from './nav/nav.component';
-import { MtfListComponent } from './mtf-list/mtf-list.component';
-import { ImageSliderComponent } from './image-slider/image-slider.component';
 
-export const components = [
-  ImageSliderComponent,
-  MtfListComponent,
-  NavBarComponent
-]
+export const components = [NavBarComponent];
